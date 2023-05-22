7c1b1dbfe9e4937bba9c9184c97154e51d1aa36e
yuqui <kobayukapc@gmail.com>
2023-05-22 11:49:20 +0900

    ExMethod4にコメント追加

--- ExMethod4.java
+++ ExMethod4.java
@@ -8,6 +8,12 @@ public class ExMethod4{
 	int taxPrice=calcPrice(price,taxRatio);
 	System.out.printf("税込み価格は%d円です%n",taxPrice);
 	}
+	/*
+	 * calcPriceメソッド
+	 * 税抜き価格と。消費税率から税込み価格を求める
+	 * 引数:price(int)税抜き価格、taxRatio(int)税率
+	 * 戻り値:taxPrice(int)税込み価格
+	 */
 	public static int calcPrice(int price,int taxRatio){
 		int taxPrice=(int)(price*(1+taxRatio/100.0));
 		return taxPrice;
